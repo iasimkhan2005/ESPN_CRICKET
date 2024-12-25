ef7e2ab12642a74763dd71e4079e7e8bf7b7fb21
Administrator <asimali9596a@gmail.com>
2024-12-25 18:02:03 +0500

    First Commit

---
+++ build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ESPN_Cricket_FILE C:/Users/Administrator/OneDrive - Higher Education Commission/Bahria Uni/3rd semester/DSA/Project/ESPN_Cricket/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/ESPN_Cricket.exe)
+set(__QT_DEPLOY_TARGET_ESPN_Cricket_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ESPN_Cricket_RUNTIME_DLLS D:/Softwares/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;D:/Softwares/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;D:/Softwares/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
