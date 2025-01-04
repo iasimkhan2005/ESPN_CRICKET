1659765081113a3769241cffab0a21099f804676
Administrator <asimali9596a@gmail.com>
2025-01-04 18:00:52 +0500

    Added the Api to Fetch Data of current Ongoing matches and also finished matches( Priority Queue )

--- championtrophy.cpp
+++ championtrophy.cpp
@@ -4,24 +4,32 @@
 ChampionTrophy::ChampionTrophy(tournamentScreen* tournament, QWidget *parent)
     : QDialog(parent), ui(new Ui::ChampionTrophy), tournament(tournament)
 {
+
     ui->setupUi(this);
-    populateTeams(); // Populate the dropdown during initialization
+    populateTeams();
+
 }
 
-void ChampionTrophy::populateTeams() {
-    if (!tournament) return; // Ensure the tournament pointer is valid
+void ChampionTrophy::populateTeams()
+{
+    if (!tournament) return;
 
-    vector<string> teams = tournament->getAllTeams(); // Get teams from tournament
+    vector<string> teams = tournament->getAllTeams();
     QStringList teamList;
-    for (const auto& teamName : teams) {
+
+    for (const auto& teamName : teams)
+    {
         teamList << QString::fromStdString(teamName);
     }
 
-    ui->comboBox->addItems(teamList); // Populate the combo box
- connect(ui->comboBox, &QComboBox::currentTextChanged, this, &ChampionTrophy::onTeamSelected);
+    ui->comboBox->addItems(teamList);
+
+     connect(ui->comboBox, &QComboBox::currentTextChanged, this, &ChampionTrophy::onTeamSelected);
+
 }
 
-void ChampionTrophy::onTeamSelected(const QString& teamName) {
+void ChampionTrophy::onTeamSelected(const QString& teamName)
+{
     if (!tournament) return;
 
     // Clear previous match details
@@ -32,23 +40,29 @@ void ChampionTrophy::onTeamSelected(const QString& teamName) {
 
     // Display match details
     QString matchDetails;
-    for (const auto& match : matches) {
-        if(teamName.toStdString()!=match.team2){
-        matchDetails += QString("Opponent: %1\nDate: %2\nStadium: %3\n\n")
-        .arg(QString::fromStdString(match.team2))
-        .arg(QString::fromStdString(match.dateTime))
-        .arg(QString::fromStdString(match.stadium));
-        }else if(teamName.toStdString()!=match.team1){
+    for (const auto& match : matches)
+    {
+        if(teamName.toStdString()!=match.team2)
+            {
             matchDetails += QString("Opponent: %1\nDate: %2\nStadium: %3\n\n")
-            .arg(QString::fromStdString(match.team1))
+            .arg(QString::fromStdString(match.team2))
             .arg(QString::fromStdString(match.dateTime))
             .arg(QString::fromStdString(match.stadium));
-        }
+            }
+
+        else if(teamName.toStdString()!=match.team1)
+            {
+                matchDetails += QString("Opponent: %1\nDate: %2\nStadium: %3\n\n")
+                .arg(QString::fromStdString(match.team1))
+                .arg(QString::fromStdString(match.dateTime))
+                .arg(QString::fromStdString(match.stadium));
+            }
     }
 
-    // Show the match details in the text area
+
     ui->matchdetails->setText(matchDetails);
 }
+
 ChampionTrophy::~ChampionTrophy() {
     delete ui;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,8 +4,43 @@
 #include <QMainWindow>
 #include <QGraphicsView>
 #include <QGraphicsScene>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QDateTime>
+#include <QString>
+#include<queue>
+#include<vector>
+
+using namespace std;
 
 
+
+struct Matchdetail {
+    QString id;
+    QString name;
+    QString status;
+    QDateTime dateTime;
+    bool ongoing;
+
+
+    Matchdetail(const QString &id, const QString &name, const QString &status, const QDateTime &dateTime, bool ongoing)
+        : id(id), name(name), status(status), dateTime(dateTime), ongoing(ongoing) {}
+
+};
+
+struct MatchComparator {
+    bool operator()(const Matchdetail &m1, const Matchdetail &m2) {
+        // Ongoing matches have higher priority
+        if (m1.ongoing != m2.ongoing)
+            return !m1.ongoing; // false < true, so ongoing comes first
+
+        // If both are ongoing or not, sort by date (most recent first)
+        return m1.dateTime < m2.dateTime;
+    }
+};
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -22,18 +57,25 @@ public:
     ~MainWindow();
 
 private slots:
-    void onTournamentButtonClicked();
-   void showTournamentsScreen();
-    //void showICCRankingScreen();
-    //void showPlayerCardsScreen();
-    //void showMatchScheduleScreen();
 
-private:
+    void showTournamentsScreen();
+
+private slots:
+   void fetchMatchData();
+   void handleApiResponse(QNetworkReply *reply);
+   void updateMatchList();
+
+
+
+   private:
     Ui::MainWindow *ui;
-    QGraphicsScene *mainScene;
-   // QGraphicsScene *tournamentScene;
-    QGraphicsScene *dataScene;
-     QGraphicsScene *tournamentScene;
+
+    QNetworkAccessManager *networkManager;
+    QString apiKey;
+
+    // Priority queue for matches
+    priority_queue<Matchdetail, std::vector<Matchdetail>, MatchComparator> matchQueue;
 };
+
 
 #endif // MAINWINDOW_H
